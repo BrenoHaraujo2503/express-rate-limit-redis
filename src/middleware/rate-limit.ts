2671130a0ca6d4bf80e0170a1e2e04eecd873c27
Breno Henrique <brenoharaujomorais@gmail.com>
2023-01-09 18:16:04 -0300

    get ip by header

--- src/middleware/rate-limit.ts
+++ src/middleware/rate-limit.ts
@@ -2,17 +2,19 @@ import { NextFunction, Request, Response } from "express";
 import { redis } from "../libs/cache";
 
 export async function RateLimit(request: Request, response: Response, next: NextFunction) {
-  const requestsByIp = await redis.get(request.ip)
+  const ip = request.headers['x-forwarded-for'] || request.ip
+  if (!ip) return;
+  const requestsByIp = await redis.get(ip.toString())
   if (requestsByIp) {
     if (requestsByIp.split(",").length >= 5) {
-      redis.expire(request.ip, 30)
+      redis.expire(ip.toString(), 30)
       return response.send("Rate limit")
     }
-    redis.set(request.ip, Array(requestsByIp, Date.now().toString()).toString())
-    redis.expire(request.ip, 5)
+    redis.set(ip.toString(), Array(requestsByIp, Date.now().toString()).toString())
+    redis.expire(ip.toString(), 5)
   } else {
-    redis.set(request.ip, Date.now().toString())
-    redis.expire(request.ip, 5)
+    redis.set(ip.toString(), Date.now().toString())
+    redis.expire(ip.toString(), 5)
   }
   console.log(requestsByIp)
   next()
