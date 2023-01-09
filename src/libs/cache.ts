de79ce32808a75995506560dca90ae471f6c3acf
Breno Henrique <brenoharaujomorais@gmail.com>
2023-01-09 18:08:41 -0300

    change redis connect for url

--- src/libs/cache.ts
+++ src/libs/cache.ts
@@ -1,10 +1,5 @@
 import Redis from 'ioredis'
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD
-});
+const redis = new Redis(process.env.REDIS_URL || "");
 
 export { redis };
--- src/middleware/rate-limit.ts
+++ src/middleware/rate-limit.ts
@@ -4,15 +4,15 @@ import { redis } from "../libs/cache";
 export async function RateLimit(request: Request, response: Response, next: NextFunction) {
   const requestsByIp = await redis.get(request.ip)
   if (requestsByIp) {
-    if (requestsByIp.split(",").length >= 10) {
-      redis.expire(request.ip, 60)
+    if (requestsByIp.split(",").length >= 5) {
+      redis.expire(request.ip, 30)
       return response.send("Rate limit")
     }
     redis.set(request.ip, Array(requestsByIp, Date.now().toString()).toString())
-    redis.expire(request.ip, 10)
+    redis.expire(request.ip, 5)
   } else {
     redis.set(request.ip, Date.now().toString())
-    redis.expire(request.ip, 10)
+    redis.expire(request.ip, 5)
   }
   console.log(requestsByIp)
   next()
